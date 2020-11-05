5e5598b674189a3b707e520f06f7194da503438f
sh667 <sh667@njit.edu>
2020-11-05 12:57:50 -0500

    Feature:added the Variance operation to the Statistics Calculator

--- Statistics/Mean.py
+++ Statistics/Mean.py
@@ -2,7 +2,7 @@ from Calculator.Addition import addition
 from Calculator.Division import division
 
 
-def mean(data):
+def mean1(data):
     num_values = len(data)
     total = 0
     for num in data:
