298867f55d37b9c679c255c8ddb2fac9a805215e
sh667 <sh667@njit.edu>
2020-11-06 08:07:10 -0500

    Feature: Added variance file updates in Statistics Calculator

--- Statistics/Variance.py
+++ Statistics/Variance.py
@@ -1,16 +1,19 @@
 from Statistics.Mean import get_mean
 from Calculator.Division import division
-
-
+from Calculator.Addition import addition
+from Calculator.Subtraction import subtraction
+from Calculator.Square import square
 def get_variance(data):
     x1 = get_mean(data)
     num_values = len(data)
     total = 0
     total1 = 0
     data1 = []
-    for i in data[0:]:
+    for i in range[0,len(data)]:
         a = data[i - 1]
-        total = (a - x1) ** 2
+        total_sum = subtraction(a, x1)
+        total = square(total_sum)
         data1.append(total)
-    total1 = sum(data1)
+    for i in range(0, len(data1)):
+        total1 = total1 + addition(0, data1[i])
     return division(num_values - 1, total1)
