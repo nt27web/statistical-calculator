0ace2c2ee31b41235192ffd8a13f37fc7a388cba
Nayana Kumari <70977839+nt27web@users.noreply.github.com>
2020-11-08 20:51:35 -0500

    Fix- updated Calculator test cases

--- Tests/test_CalculatorTest.py
+++ Tests/test_CalculatorTest.py
@@ -65,9 +65,10 @@ class MyTestCase(unittest.TestCase):
     def test_subtraction(self):
         test_data = CsvReader("Tests/Data/UnitTestSubtraction.csv").data
         for row in test_data:
-            result = float(row['Result'])
-            self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), result)
-            self.assertEqual(self.calculator.result, result)
+            result_float = float(row['Result'])
+            self.assertEqual(self.calculator.subtract(float(row['Value 1']), float(row['Value 2'])), result_float)
+            result_int = int(row['Result'])
+            self.assertEqual(self.calculator.subtract(int(row['Value 1']), int(row['Value 2'])), result_int)
 
     def test_addition(self):
         test_data = CsvReader("Tests/Data/UnitTestAddition.csv").data
@@ -79,30 +80,34 @@ class MyTestCase(unittest.TestCase):
     def test_multiplication(self):
         test_data = CsvReader("Tests/Data/UnitTestMultiplication.csv").data
         for row in test_data:
-            result = float(row['Result'])
-            self.assertEqual(self.calculator.multiply(row['Value 1'], row['Value 2']), result)
-            self.assertEqual(self.calculator.result, result)
+            result_float = float(row['Result'])
+            self.assertEqual(self.calculator.multiply(float(row['Value 1']), float(row['Value 2'])), result_float)
+            result_int = int(row['Result'])
+            self.assertEqual(self.calculator.multiply(int(row['Value 1']), int(row['Value 2'])), result_int)
 
     def test_division(self):
         test_data = CsvReader("Tests/Data/UnitTestDivision.csv").data
         for row in test_data:
-            result = float(row['Result'])
-            self.assertEqual(self.calculator.divide(row['Value 1'], row['Value 2']), result)
-            self.assertEqual(self.calculator.result, result)
+            result_float = float(row['Result'])
+            self.assertEqual(self.calculator.divide(float(row['Value 1']), float(row['Value 2'])), result_float)
+            result_int = float(row['Result'])
+            self.assertEqual(self.calculator.divide(int(row['Value 1']), int(row['Value 2'])), result_int)
 
     def test_square(self):
         test_data = CsvReader("Tests/Data/UnitTestSquare.csv").data
         for row in test_data:
-            result = float(row['Result'])
-            self.assertEqual(self.calculator.square(row['Value 1']), result)
-            self.assertEqual(self.calculator.result, result)
+            result_float = float(row['Result'])
+            self.assertEqual(self.calculator.square(float(row['Value 1'])), result_float)
+            result_int = int(row['Result'])
+            self.assertEqual(self.calculator.square(int(row['Value 1'])), result_int)
 
     def test_square_root(self):
         test_data = CsvReader("Tests/Data/UnitTestSquareRoot.csv").data
         for row in test_data:
-            result = float(row['Result'])
-            self.assertEqual(self.calculator.square_root(row['Value 1']), result)
-            self.assertEqual(self.calculator.result, result)
+            result_float = float(row['Result'])
+            self.assertEqual(round(self.calculator.square_root(float(row['Value 1'])) , 8), result_float)
+            result_int = float(row['Result'])
+            self.assertEqual(round(self.calculator.square_root(int(row['Value 1'])) , 8), result_int)
 
 
 if __name__ == '__main__':
