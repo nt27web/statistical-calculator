3dec28d490f78e1954dbb39714d9473eac89960b
Nayana Kumari <70977839+nt27web@users.noreply.github.com>
2020-11-08 21:27:18 -0500

    Fix- added and modified random generator test cases.

--- Tests/test_RandomGenerator.py
+++ Tests/test_RandomGenerator.py
@@ -11,18 +11,26 @@ class MyTestCase(unittest.TestCase):
 
     # Generate a random number without a seed between a range of two numbers - Both Integer and Decimal
     def test_generate_rand_num_by_range_wo_seed(self):
-        print("function - test_generate_rand_num_by_range_wo_seed()")
-        print(self.random_generator.generate_rand_num_by_range_wo_seed(3, 5))
+        #print("function - test_generate_rand_num_by_range_wo_seed()")
+        #print(self.random_generator.generate_rand_num_by_range_wo_seed(3, 5))
+        self.assertLessEqual(self.random_generator.generate_rand_num_by_range_wo_seed(3, 5), 5)
+        self.assertGreaterEqual(self.random_generator.generate_rand_num_by_range_wo_seed(3, 5), 3)
 
     # Generate a random number with a seed between a range of two numbers - Both Integer and Decimal
     def test_generate_rand_num_by_range_w_seed(self):
-        print("function - test_generate_rand_num_by_range_w_seed()")
-        print(self.random_generator.generate_rand_num_by_range_w_seed(5, 2, 3, 5))
+        #print("function - test_generate_rand_num_by_range_w_seed()")
+        #print(self.random_generator.generate_rand_num_by_range_w_seed(5, 2, 3, 5))
+        self.assertLessEqual(self.random_generator.generate_rand_num_by_range_w_seed(5, 2, 3, 5), 5)
+        self.assertGreaterEqual(self.random_generator.generate_rand_num_by_range_w_seed(5, 2, 3, 5), 3)
 
     # Generate a list of N random numbers with a seed and between a range of numbers - Both Integer and Decimal
     def test_get_rand_num_list_by_range_w_seed(self):
-        print("function - test_get_rand_num_list_by_range_w_seed()")
-        print(self.random_generator.get_rand_num_list_by_range_w_seed(5, 2, 2, 1, 9))
+        #print("function - test_get_rand_num_list_by_range_w_seed()")
+        #print(self.random_generator.get_rand_num_list_by_range_w_seed(5, 2, 2, 1, 9))
+        data = self.random_generator.get_rand_num_list_by_range_w_seed(5, 2, 2, 1, 9)
+        for num in range(len(data)):
+            self.assertLessEqual(data[num], 9)
+            self.assertGreaterEqual(data[num], 1)
 
     # Set a seed and randomly.select the same value from a list
     def test_set_seed_and_get_rand_from_list(self):
@@ -40,13 +48,15 @@ class MyTestCase(unittest.TestCase):
     def test_get_rand_num_list_wo_seed(self):
         #print("function - test_get_rand_num_list_wo_seed()")
         #print(self.random_generator.get_rand_num_list_wo_seed(4, [5, 3, 2, 1, 9]))
-        self.assertTrue(self.random_generator.get_rand_num_list_wo_seed(4, [5, 3, 2, 1, 9]) in [5, 3, 2, 1, 9])
+        self.assertEqual(len(self.random_generator.get_rand_num_list_wo_seed(3, [5, 3, 2, 1, 9])), 3)
+        self.assertTrue(set(self.random_generator.get_rand_num_list_wo_seed(3, [5, 3, 2, 1, 9])).issubset(set([5, 3, 2, 1, 9])))
 
     # Select N number of items from a list with a seed
     def test_get_rand_num_list_w_seed(self):
         #print("function - test_get_rand_num_list_w_seed()")
         #print(self.random_generator.get_rand_num_list_w_seed(3, 2, 2, [5, 3, 2, 1, 9]))
-        self.assertEqual(self.random_generator.get_rand_num_list_w_seed(3, 2, 2, [5, 3, 2, 1, 9]), 3)
+        self.assertEqual(len(self.random_generator.get_rand_num_list_w_seed(3, 2, 2, [5, 3, 2, 1, 9])), 3)
+        self.assertTrue(set(self.random_generator.get_rand_num_list_w_seed(3, 2, 2, [5, 3, 2, 1, 9])).issubset(set([5, 3, 2, 1, 9])))
 
 
 if __name__ == '__main__':
